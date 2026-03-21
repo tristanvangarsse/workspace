6479242bbac80eb833e235b39276068fae454f6d
tristanvangarsse <159120739+tristanvangarsse@users.noreply.github.com>
2026-03-21 14:02:45 +0100

    Removed all files (after transfer to main site)

--- dist/js/lazyload.js
+++
@@ -1,6 +0,0 @@
-let images = document.querySelectorAll(".lazyload");
-new LazyLoad(images, {
-    root: null,
-    rootMargin: "100px",
-    threshold: 0,
-});
--- dist/js/page-loader-wait.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", () => {
-    const loader = document.querySelector(".page-loader-main");
-    if (loader) {
-        loader.classList.add("hidden");
-        setTimeout(() => loader.remove(), 1);
-    }
-});
